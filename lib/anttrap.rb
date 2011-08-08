6c900a507965bfb4955597d23522224fae37bf99
Dave Parfitt <diparfitt@gmail.com>
2011-08-08 16:18:47 -0400

    added sample rakefile

--- lib/anttrap.rb
+++ lib/anttrap.rb
@@ -1,1 +1,25 @@
-puts "AntTrap"
+require 'rubygems'
+require 'builder'
+
+RAKE='rake'
+ANTFILE='build.xml'
+
+task :anttrap do
+  
+  xml = Builder::XmlMarkup.new( :indent => 2 )
+  xml.instruct! :xml, :encoding => "ASCII"  
+  proj = xml.project("name" => "Rakefile") do |p|
+    Rake::Task.tasks().each do |task|
+      p.target("name"=>task) do |t|
+        t.exec("executable"=>RAKE) do |e|
+          e.arg("value"=>task)
+        end
+      end
+    end
+  end
+  File.open(ANTFILE,"w+") do |f|
+    f.puts proj
+  end
+end
+
+
